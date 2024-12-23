488b64a1f92c310a81beda7832512016194302e5
Евгений <info-best@bk.ru>
2024-12-23 17:55:04 +0300

    idea1

--- Module_16_3.py
+++ Module_16_3.py
@@ -10,7 +10,10 @@ users = {}
 async def read_users():
     return users
 @app.post('/user/{username}/{age}')
-async def create_user(username: str, age: int):
+async def create_user( username: Annotated[str, Path(min_length=5, max_length=20,
+                                            description="Введите имя пользователя",example="UrbanProfi" )],
+                       age: Annotated[int, Path(ge=18, le=76, description="Enter User age",
+                                                         example=24)]):
     # Определяем новый ID на основе максимального ключа
     new_id = str(max(map(int, users.keys()), default=0) + 1)
     user_id = f"Имя: {username}, возраст: {age}"
@@ -30,11 +33,12 @@ async def update_user(user_id: Annotated[int, Path(ge=1, le=100, description="В
         users[str(user_id)] = user_info
         return f"Пользователь {user_id} именен на: Имя {username}, возраст {age}"
 @app.delete('/user/{user_id}')
-async def delete_user(user_id: int, username: str):
+async def delete_user(user_id: Annotated[int, Path(ge=1, le=100, description="Enter User id",
+                                                         example=24)] ):
 
     if str(user_id) in users:
         del users[str(user_id)]
-        return f"Пользователь с Id номером {user_id} имя {username} удален"
+        return f"Пользователь с Id номером {user_id}  удален"
     else:
         return f"Пользователь с ID номером {user_id} отсутствует"
 
